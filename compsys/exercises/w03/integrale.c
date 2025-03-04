26495fb90d935f131d1dba6e45226696ad55f6c1
Androz2091 <androz2091@gmail.com>
2025-03-04 13:12:56 +0100

    token

--- compsys/exercises/w03/integrale.c
+++ compsys/exercises/w03/integrale.c
@@ -1,8 +1,59 @@
 #include <stdio.h>
 #include <math.h>
 
-double f(double x) {
+double s(double x) {
   return sin(x);
+}
+
+double e(double x) {
+    return exp(x);
+}
+
+double f1(double x) {
+    return log10(x) + x;
+}
+
+double f2(double x) {
+    return exp(sqrt(x));
+}
+
+double f3(double x) {
+    return exp(sqrt(x + 8));
+}
+
+typedef double (*Function)(double);
+
+Function request_function() {
+    printf("Please enter a number between 1-5: ");
+    int res = 0;
+    scanf("%d", &res);
+    Function func = NULL;
+    switch (res) {
+        case 1: {
+            func = f1;
+            break;
+        }
+        case 2: {
+            func = f2;
+            break;
+        }
+        case 3: {
+            func = f3;
+            break;
+        }
+        case 4: {
+            func = s;
+            break;
+        }
+        case 5: {
+            func = e;
+            break;
+        }
+        default: {
+            printf("what are you doing?");
+        }
+    }
+    return func;
 }
 
 double demander_nombre(void)
@@ -13,7 +64,7 @@ double demander_nombre(void)
   return res;
 }
 
-double integre(double a, double b)
+double integre(double a, double b, Function f)
 {
   double res =
        41.0 * ( f(a) + f(b) )
@@ -32,6 +83,6 @@ int main(void)
   double
     a = demander_nombre(),
     b = demander_nombre();
-  printf("Integrale de sin(x) entre %f et %f : %.12f\n", a, b, integre(a, b));
+  printf("Integrale de f entre %f et %f : %.12f\n", a, b, integre(a, b, request_function()));
   return 0;
 }
